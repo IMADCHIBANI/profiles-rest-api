d39950f7bb25ea0c7a5ff2bb9642555380163f21
unknown <imadchibani@gmail.com>
2022-04-16 16:39:17 +0200

    Added POST,PUT,PATCH,DELETE

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -22,10 +22,21 @@ class HelloApiView(APIView):
         serializer= self.serializer_class(data=request.data)
         if serializer.is_valid():
             name=serializer.validated_data.get('name')
-            message=f'Hello (name)'
+            message=f'Hello {name}'
             return Response({'message':message})
         else:
             return Response(
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
                 )
+    def put(self,request,pk=None):
+        """ Handle updating an object"""
+        return Response({'method':'PUT'})
+
+    def patch(self,request,pk=None):
+        """Handle a partial update of an object """
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
